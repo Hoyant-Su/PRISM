a754f4cc36881a3db9951c610da0eb6553a1175c
Hoyant_Su <shy3765@163.com>
2025-09-27 15:59:40 +0000

    Add detailed info of CTSL

--- push.sh
+++ push.sh
@@ -3,4 +3,4 @@ git config --global user.name "Hoyant_Su"
 cd /inspire/hdd/project/continuinglearning/suhaoyang-240107100018/suhaoyang-240107100018/research/PRISM
 git add .
 git commit -m "Add detailed info of CTSL"
-git push --set-upstream origin main
+git push --set-upstream origin master

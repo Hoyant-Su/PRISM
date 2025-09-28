554169c25c59dfd5ad58da5ec70edc5164b53e4c
Hoyant_Su <shy3765@163.com>
2025-09-28 08:38:03 +0000

    Add detailed info for README.

--- push.sh
+++
@@ -1,6 +0,0 @@
-git config --global user.email "shy3765@163.com"
-git config --global user.name "Hoyant_Su"
-cd /inspire/hdd/project/continuinglearning/suhaoyang-240107100018/suhaoyang-240107100018/research/PRISM
-git add .
-git commit -m "Add detailed info for README."
-git push --set-upstream origin master
